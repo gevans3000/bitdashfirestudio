f7848f43f67e4840d332753b89fe8ae72c791269
G <71615719+gevans3000@users.noreply.github.com>
2025-06-04 08:25:13 -0400

    fix(memory): add lock to commit log (#170)

--- scripts/commit-log.ts
+++ scripts/commit-log.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import { memPath, readMemoryLines, atomicWrite } from './memory-utils';
+import { memPath, readMemoryLines, atomicWrite, withFileLock } from './memory-utils';
 
 const log = readMemoryLines().slice(-20).join('\n');
 const outPath = path.join(__dirname, '../logs/commit.log');
-atomicWrite(outPath, `${log}\n`);
+withFileLock(outPath, () => {
+  atomicWrite(outPath, `${log}\n`);
+});
 console.log(`Commit log written to ${outPath}`);
